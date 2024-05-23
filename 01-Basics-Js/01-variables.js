fdc402654f520383ed6b8ca559e6d1d82cceba8f
Aqdas Ali <163125941+aqdas-balti@users.noreply.github.com>
2024-05-23 08:26:31 +0000

    Variables Practice

---
+++ 01-Basics-Js/01-variables.js
@@ -0,0 +1,10 @@
+const account_Id = 36281;
+let acoount_Name = "Aqdas Ali";
+var account_Email = "aqdasali9999@gmail.com";
+city = "Islamabad";
+// console.log("This is a const variable",account_Id);
+// console.log("This is a let variable",account_Name);
+// console.log("This is a var variable",account_Email);
+// console.log("This is undefined variable",city);
+// Display all the variables with one
+console.log([account_Id,account_Name,account_Email,city]);

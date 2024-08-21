f2a4a8a2b7b2f93b933313f045d7e5948a04eb0d
aqdas-balti <aqdasali584@gmail.com>
2024-08-21 01:13:58 -0700

    ArrowFun

--- 03-Basics/03_ArrowFun.js
+++ 03-Basics/03_ArrowFun.js
@@ -4,6 +4,12 @@ const user={
     price:999,
     wellcomeMessage :function(){
         console.log(`${this.userName}, WellCome To Website`);
-        
-    }
+        // console.log(this); //Nothing print 
+    }  
 }
+// console.log(this);//{}
+
+user.wellcomeMessage();//console.log(this);
+//(This) is the context of a function
+user.userName = "Ali";
+user.wellcomeMessage()//console.log(this);

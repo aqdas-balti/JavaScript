fdd139f1248154e59020bc44284d844373039e63
aqdas-balti <aqdasali584@gmail.com>
2024-08-22 01:24:55 -0700

    thisContext

--- 03-Basics/03_ArrowFun.js
+++ 03-Basics/03_ArrowFun.js
@@ -14,8 +14,28 @@ const user={
 // user.userName = "Ali";
 // user.wellcomeMessage()//console.log(this);
 
-function chai(){
-    console.log(this);
+//We can use our context (this) is only in the object not in the function thts why it will display [undefined]
+//const chai = function (){} another method to delclare the function
+// function chai(){
+//     let userName="BOB"
+//     console.log(this.userName);
     
-}
-chai();
+// }
+// chai();//undefined
+
+//Arroe Function can we do it in it?
+// const chai =  () =>{
+//     let userName="BOB"
+//     console.log(this.userName);
+// }
+// chai();//undefined ans will be same
+
+// const addTwo=(num1,num2)=>{
+//     return num1+num2;
+// }
+// console.log(addTwo(2,4));//6
+
+//Implicit return of the arrow function
+
+const addTwo=(num1,num2)=> num1+num2;
+    console.log(addTwo(2,4));//6

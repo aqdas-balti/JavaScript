c9e504b17e70b9b1ba3815a9e56efe8d2d9af25f
aqdas-balti <aqdasali584@gmail.com>
2024-08-22 01:07:08 -0700

    thisContext

--- 03-Basics/03_ArrowFun.js
+++ 03-Basics/03_ArrowFun.js
@@ -4,12 +4,18 @@ const user={
     price:999,
     wellcomeMessage :function(){
         console.log(`${this.userName}, WellCome To Website`);
-        // console.log(this); //Nothing print 
+        console.log(this); //Nothing print 
     }  
 }
-// console.log(this);//{}
+// console.log(this);//{} because of node environment
 
-user.wellcomeMessage();//console.log(this);
-//(This) is the context of a function
-user.userName = "Ali";
-user.wellcomeMessage()//console.log(this);
+// user.wellcomeMessage();//console.log(this);
+// //(This) is the context of a function
+// user.userName = "Ali";
+// user.wellcomeMessage()//console.log(this);
+
+function chai(){
+    console.log(this);
+    
+}
+chai();

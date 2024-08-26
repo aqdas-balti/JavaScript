c9a4dd90bdbf4dab7abef3928f20c5af2dddc6bf
aqdas-balti <aqdasali584@gmail.com>
2024-08-25 23:55:35 -0700

    If Condition

--- 04-Control-Flow/one.js
+++ 04-Control-Flow/one.js
@@ -1,23 +1,23 @@
 //If Statement
-// const isloggedIn = true;
-// if (isloggedIn){
-//     console.log("Logged In");
+const isloggedIn = true;
+if (isloggedIn){
+    console.log("Logged In");
     
-// }
-// const temperature = 41;
+}
+const temperature = 41;
 
-// if (temperature>40){
-//     console.log("Temperature is Grater Than 40");
+if (temperature>40){
+    console.log("Temperature is Grater Than 40");
     
-// }
-// else{
-//     console.log("Less Than 40");
+}
+else{
+    console.log("Less Than 40");
     
-// }
+}
 
-// const score = 100;
-// if (score>50){
-//     const power = "Fly";
-//     console.log(`User Power ${power}`);
+const score = 100;
+if (score>50){
+    const power = "Fly";
+    console.log(`User Power ${power}`);
     
 }

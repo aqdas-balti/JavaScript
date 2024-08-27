9f8e1892b45e24c576abb0e7af6175d12d6540ce
aqdas-balti <aqdasali584@gmail.com>
2024-08-27 00:58:03 -0700

    Switch Case

---
+++ 04-Control-Flow/switch.js
@@ -0,0 +1,25 @@
+//Switch statements
+const Month=3;
+switch (Month) {
+    case 1:
+        console.log("January");
+        
+        break;
+    case 2:
+        console.log("Feb.");
+        
+        break;
+    case 3:
+        console.log("March");
+        
+        break;
+    case 4 :
+        console.log("April");
+        
+        break;
+
+    default:
+        console.log("Default Case");
+        
+        break;
+}

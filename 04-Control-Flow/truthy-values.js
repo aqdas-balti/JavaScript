d27eb374ef04b240334613a7df27716b6fdd5932
aqdas-balti <aqdasali584@gmail.com>
2024-08-28 00:44:27 -0700

    Truty Values

---
+++ 04-Control-Flow/truthy-values.js
@@ -0,0 +1,9 @@
+//Truthy values are the values that we assumed that it is true by default if some thing in the variables then it will display the true values
+//Falsy Values if nothing in the variable then it will display the false values ans
+
+let Email = "aqdasali584@gmail.com"
+if (Email) {
+    console.log("Yes email is declared");
+}else{
+    console.log("No email is not declare");   
+}

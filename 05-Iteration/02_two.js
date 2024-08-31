c65e547b045b0a1606a76c22e58adad9fdfbb09f
aqdas-balti <aqdasali584@gmail.com>
2024-08-31 00:30:49 -0700

    While Loop & Do While Loop

--- 05-Iteration/02_two.js
+++ 05-Iteration/02_two.js
@@ -1,10 +1,11 @@
 //While Loop
+//Simple While Loop
 let index = 0;
 while (index<=10) {
     console.log(`The Value of Index is: ${index}`);
     index=index+2;
 }
-
+//While with Array
 let myArray = ["Aqdas","Anwar","Abdullah","Furqan","Irfan"];
 let arr=0;
 console.log("Total Length of myArray is ",myArray.length);//5
@@ -13,3 +14,17 @@ while (arr<myArray.length) {
     arr=arr+1; 
 }
 
+//Do While Loop first print statement then check the condition
+//Simple Do While Loop
+let score = 1;
+do {
+    console.log(`The value of score is: ${score}`);
+    score++;
+} while (score<=10);
+//Do While with Array
+let myArray2=["Aqdas","Anwar","Abdullah","Furqan","Irfan"];
+let arr2 =1;
+do {
+    console.log(`The value of myArray2 is : ${myArray2[arr2]}`);
+    arr2=arr2+1;
+} while (arr2<myArray2.length);

8227f64639d8eeae4f5f2b669a6e723913953f75
aqdas-balti <aqdasali584@gmail.com>
2024-08-31 00:17:47 -0700

    While Loop

---
+++ 05-Iteration/02_two.js
@@ -0,0 +1,15 @@
+//While Loop
+let index = 0;
+while (index<=10) {
+    console.log(`The Value of Index is: ${index}`);
+    index=index+2;
+}
+
+let myArray = ["Aqdas","Anwar","Abdullah","Furqan","Irfan"];
+let arr=0;
+console.log("Total Length of myArray is ",myArray.length);//5
+while (arr<myArray.length) {
+    console.log(`The value of myArray is: ${myArray[arr]}`);  
+    arr=arr+1; 
+}
+

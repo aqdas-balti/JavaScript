1a47e4d604c4d593f51b997ece67145cb635f38e
aqdas-balti <aqdasali584@gmail.com>
2024-08-31 23:34:40 -0700

    FOr Of Loop

--- 05-Iteration/03_three.js
+++ 05-Iteration/03_three.js
@@ -7,16 +7,16 @@ for (const value of arr) {
     console.log(value);//1 to 5 Prints
 }
 
-//Array With Strings
-let arr2 = ["Aqdas","Anwar","Abdullah","Furqan","Irfan"];
-for (const val2 of arr2) {
-    console.log(val2);
-}
+// //Array With Strings
+// let arr2 = ["Aqdas","Anwar","Abdullah","Furqan","Irfan"];
+// for (const val2 of arr2) {
+//     console.log(val2);
+// }
 
-let greetings = "Hello World!"
-for (const greet of greetings) {
-    if (greet==" ") {//This will neglect the space of the array
-        continue
-    }
-    console.log(`Each Char is ${greet}`);
-}
+// let greetings = "Hello World!"
+// for (const greet of greetings) {
+//     if (greet==" ") {//This will neglect the space of the array
+//         continue
+//     }
+//     console.log(`Each Char is ${greet}`);
+// }

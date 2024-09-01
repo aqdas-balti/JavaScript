d54725d1c7857b6bc4ecff0272e79d85bb6c056a
aqdas-balti <aqdasali584@gmail.com>
2024-09-01 10:45:10 -0700

    FOr Of Loop

--- 05-Iteration/03_three.js
+++ 05-Iteration/03_three.js
@@ -4,13 +4,13 @@
 //Array With Normal Integers
 let arr = [1,2,3,4,5]
 for (const value of arr) {
-    console.log(value);//1 to 5 Prints
+    // console.log(value);//1 to 5 Prints
 }
 
 //Array With Strings
 let arr2 = ["Aqdas","Anwar","Abdullah","Furqan","Irfan"];
 for (const val2 of arr2) {
-    console.log(val2);
+    // console.log(val2);
 }
 
 let greetings = "Hello World!"
@@ -18,5 +18,31 @@ for (const greet of greetings) {
     if (greet==" ") {//This will neglect the space of the array
         continue
     }
-    console.log(`Each Char is ${greet}`);
+    // console.log(`Each Char is ${greet}`);
+}
+
+//Maps
+const map = new Map();
+map.set('PK','Pakistan');
+map.set('IR','Iran');
+map.set('IN','India');
+map.set('IRQ','Iraq');
+// console.log(map);
+
+for (const key of map) {
+    console.log(key);//It will display the result with in array to avoid we use below
+}
+/*Ans:
+['PK', 'Pakistan']
+['IR', 'Iran']
+['IN', 'India']
+['IRQ', 'Iraq'] */
+
+for (const [key,value] of map) {
+    console.log(key,":-",value);//It will display the result with separate
+/*Ans:
+PK :- Pakistan
+IR :- Iran
+IN :- India
+IRQ :- Iraq */
 }

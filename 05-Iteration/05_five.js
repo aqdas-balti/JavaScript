4e16dbdf4890b2d0cbaa6167a09a7c62dfbcdfe0
aqdas-balti <aqdasali584@gmail.com>
2024-09-03 10:08:42 -0700

    Foreach loop

--- 05-Iteration/05_five.js
+++ 05-Iteration/05_five.js
@@ -2,21 +2,21 @@
 const coding = ["JS","PY","CPP","RB"];
 console.log("Printed the items with  function call");
 coding.forEach( function (items){
-    console.log(items); //JS PY CPP RB will be print 
+    // console.log(items); //JS PY CPP RB will be print 
 })
 //Use arrow function in the foreach loop
 console.log("Printed the items with arrow function call");
 coding.forEach ( (values) => {
-    console.log(values);//JS PY CPP RB will be print 
+    // console.log(values);//JS PY CPP RB will be print 
 })
 //Use function as a reference like
-// console.log("Print the values with reference function");
-// function printMe(ref){
-//     console.log(ref);
-// }
-// coding.forEach( printMe );//JS PY CPP RB will be print
+console.log("Print the values with reference function");
+function printMe(ref){
+    // console.log(ref);
+}
+coding.forEach( printMe );//JS PY CPP RB will be print
 
-// //we can also see the value, index and also full array values
-// coding.forEach( (val,index,arr)=>{
-//     console.log(val,index,arr);
-// })
+//we can also see the value, index and also full array values
+coding.forEach( (val,index,arr)=>{
+    console.log(val,index,arr);
+})

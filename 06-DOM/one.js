04dbf47b843172556e38019c53c8ecb6f0db5637
aqdas-balti <aqdasali584@gmail.com>
2024-09-10 13:04:46 -0700

    DOM Starts

---
+++ 06-DOM/one.js
@@ -0,0 +1,56 @@
+
+//All of this work are done in the console of the window
+
+/*document.getElementById('title').getAttribute('id')
+'title'
+document.getElementById('title').getAttribute('class')
+'Heading'
+document.getElementById('title').id
+'title'
+document.getElementById('title').className
+'Heading'
+document.getElementById('title').setAttribute('class','test')
+undefined
+document.getElementById('title').setAttribute('class','test heading')
+undefined
+document.getElementById('title').getAttribute('class')
+'test heading'
+document.getElementById('title').setAttribute('class','Heading')
+undefined
+title
+<h1 id=​"title" class=​"Heading">​Dom Learning on chai aor code​</h1>​
+title.style.backgroundColor='green'
+'green'
+title.style.borderTopLeftRadius="15px"
+'15px'
+
+Now From Here Very Important Work Will Be Start To Add a New Content in the frontend
+title.innerText
+'Dom Learning on chai aor code'
+title.textContent
+'Dom Learning on chai aor code'
+title.innerHTML
+'Dom Learning on chai aor code'
+
+title.innerHTML This will show also the tags of the html
+'Dom Learning on chai aor code <span style="display: none;">This Is Span</span>'
+title.innerText This will only show thw text
+'Dom Learning on chai aor code'
+title.textContent This will also show the hide things
+'Dom Learning on chai aor code This Is Span'
+
+document.getElementsByClassName('Heading')
+HTMLCollection [h1#title.Heading, title: h1#title.Heading]
+document.querySelector('h1')
+<h1 id=​"title" class=​"Heading">​…​</h1>​
+document.querySelector('h2')
+<h2>​1: Lorem ipsum dolor sit amet.​</h2>​
+
+Document.querySelector('#title')
+<h1 id=​"title" class=​"Heading">​…​</h1>​
+document.querySelector('.Heading')
+<h1 id=​"title" class=​"Heading">​…​</h1>​
+
+document.querySelector('input[type=password]')
+<input type=​"password" name id>​
+*/
